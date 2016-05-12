2459b95c992dd6bea723f0d0f80598eb5b28e83c
Diego Espínola <despinolaespigares@gmail.com>
2016-05-13 00:23:26 +0200

    iOS

---
+++ pachangaManager/pachangaManager/JugadoresTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  JugadoresTableViewCell.h
+//  pachangaManager
+//
+//  Created by Diego Espínola Espigares on 12/5/16.
+//  Copyright © 2016 Juan Angel y Diego Espínola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JugadoresTableViewCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UILabel *nombre;
+
+@end
---
+++ pachangaManager/pachangaManager/JugadoresTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  JugadoresTableViewCell.m
+//  pachangaManager
+//
+//  Created by Diego Espínola Espigares on 12/5/16.
+//  Copyright © 2016 Juan Angel y Diego Espínola. All rights reserved.
+//
+
+#import "JugadoresTableViewCell.h"
+
+@implementation JugadoresTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+    
+    // Configure the view for the selected state
+}
+
+@end
