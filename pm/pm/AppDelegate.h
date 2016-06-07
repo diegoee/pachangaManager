5457d60ba7645134a08a246391aa80a045283e21
Diego Espínola <despinolaespigares@gmail.com>
2016-06-07 21:00:26 +0200

    .

---
+++ pm/pm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pm
+//
+//  Created by Diego Espínola Espigares on 7/6/16.
+//  Copyright © 2016 Diego Espínola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pm/pm/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  pm
+//
+//  Created by Diego Espínola Espigares on 7/6/16.
+//  Copyright © 2016 Diego Espínola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
