96ea66a7715cb359347027e306648f927acd2d0e
Diego Espínola <despinolaespigares@gmail.com>
2016-06-11 19:21:44 +0200

    .

--- pm/pm/CompeticionCrearView.m
+++ pm/pm/CompeticionCrearView.m
@@ -30,13 +30,13 @@
 }
 
 // The number of columns of data
-- (int)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
 {
     return 1;
 }
 
 // The number of rows of data
-- (int)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
 {
     return self.pickerData.count;
 }
